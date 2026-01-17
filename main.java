193d5d6a84e6c0e450b361276bd27b4bcc977ad0
KARTHIKAKRISHNA123 <athvikaalm2018@gmail.com>
2026-01-17 14:32:11 +0530

    chore: Initial commit for Project

---
+++ main.java
@@ -0,0 +1,9 @@
+package Console_Quiz_App_using_Advance_Java;
+
+public class main{
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Console Quiz App using Advanced Java!");
+    }
+
+    
+}
